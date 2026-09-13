f80087878b9cc929ae3246b8770f149910e1d1e6
akarshit7800 <akarshitg7800@gmail.com>
2026-09-13 14:04:39 +0530

    remove services

--- services/driver-service/src/main/java/com/ridepulse/driver/repository/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ridepulse.driver.repository;
-
-import com.ridepulse.driver.entity.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DriverRepository extends JpaRepository<Driver, String> {
-    Optional<Driver> findByPhone(String phone);
-}
--- services/ml-demand-pricing/app/__init__.py
+++
@@ -1,1 +0,0 @@
-# ml-demand-pricing package
